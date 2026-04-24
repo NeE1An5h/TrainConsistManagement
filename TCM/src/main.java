d3fead1ac4781bf408e0500e0682096eb396c92d
NeE1An5h <neelanshgoyal@gmail.com>
2026-04-24 09:14:40 +0530

    UC17 Completed.

--- TCM/src/main.java
+++ TCM/src/main.java
@@ -2,24 +2,13 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("=== Bubble Sort for Passenger Bogie Capacities ===");
+        System.out.println("=== Sorting Bogie Types Alphabetically ===");
 
-        int[] capacities = {72, 50, 90, 30, 65, 80};
+        String[] bogieTypes = {"Cylindrical", "Box", "Flatbed", "Passenger", "Goods"};
 
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogieTypes);
 
-        System.out.println("Sorted Capacities:");
-
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("Sorted Bogie Types:");
+        System.out.println(Arrays.toString(bogieTypes));
     }
 }
